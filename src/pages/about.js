c420b025f9854f4fa16d658e181e54c72fa3a081
Dayne Guy <dayneguy1@outlook.com>
2021-12-18 13:15:56 -0800

    Added Links and Basic styles

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Import React
+import * as React from "react";
+import Layout from "../components/layout";
+
+// Define About Page
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+// Export Component
+export default AboutPage;
